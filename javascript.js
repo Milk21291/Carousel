56b060bef6335f117a2f6943c928d61aa9a76416
João Victor <90943414+Milk21291@users.noreply.github.com>
2021-12-08 15:20:16 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const imgs = document.getElementById('img');
+console.log(imgs);
+const img = document.querySelectorAll("#img img");
+
+
+let idx = 0;
+
+function carrosel() {
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+imgs.style.transform  = `translateX(${-idx * 2010}px)`;
+
+
+}
+
+setInterval(carrosel, 7000);
